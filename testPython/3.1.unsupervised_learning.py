16980ec608f1a30081f35f1f8edb2186632e8643
Emily-BioScience <wuyang.bnu@139.com>
2019-10-11 15:45:39 +0800

    test pca, manually

--- testPython/3.1.unsupervised_learning.py
+++ testPython/3.1.unsupervised_learning.py
@@ -37,15 +37,60 @@ def clustering_task(data, target):
     # plt.show()
     plt.savefig('output/3.1.dbscan.jpg', dpi=600)
 
+    # test roc curve, only allow 2 classes
+    fpr, tpr, thresholds = metrics.roc_curve(dbscan, kmeans)
+    plt.figure()
+    plt.plot(tpr, fpr)
+    plt.savefig('output/3.1.roc-curve.pseudo.jpg', dpi=600)
+
 
 def dimension_reduction_task(data, target):
-    pass
+    # pca for plotting
+    pcaModel = PCA(n_components=2)
+    pca = pcaModel.fit_transform(data)
+    # plot
+    red_x, red_y = [], []
+    blue_x, blue_y = [], []
+    green_x, green_y = [], []
+    for i in range(len(pca)):
+        if target[i] == 0:
+            red_x.append(pca[i][0])
+            red_y.append(pca[i][1])
+        elif target[i] == 1:
+            blue_x.append(pca[i][0])
+            blue_y.append(pca[i][1])
+        else:
+            green_x.append(pca[i][0])
+            green_y.append(pca[i][1])
+    plt.figure()
+    plt.scatter(red_x, red_y, c='r', marker='x')
+    plt.scatter(blue_x, blue_y, c='b', marker='D')
+    plt.scatter(green_x, green_y, c='g', marker='.')
+    plt.savefig('output/3.1.pca.jpg', dpi=600)
+
+    # pca manually
+    dataAdjust = data - data.mean(axis=0)  # 数据中心化
+    covmatrix = np.cov(dataAdjust.T)  # 求协方差矩阵
+    featValue, featVec = np.linalg.eig(covmatrix)  # 求特征值，特征向量
+    index = np.argsort(-featValue)  # 按特征值从大到小排序
+    k = 2 # 选择前两个主成分
+    selectVec = featVec.T[index[:k]]  # 特征向量为列向量，转置后，选取特征值靠前的k行
+    finalData = dataAdjust * np.matrix(selectVec.T)  # m * n, n* k => m * k
+                                                     # 两个np.array相乘，属于element-wise
+                                                     # 两个np.matrix相乘，属于矩阵乘法
+    reconData = (finalData * selectVec) + data.mean(axis=0)  # 还原到原始空间
+    print("手动pca，与sklearn库算出来的总体差别：{:.3f}".format((finalData - pca).sum()))
+    print("手动pca还原出的data，与原始data的总体差别：{:.3f}".format((reconData - data).sum()))
+
+
+    a, b = np.linalg.eig(covmatrix)
 
 
 
 if __name__ == '__main__':
     data, target = load_iris(return_X_y=True)
     clustering_task(data, target)
+    dimension_reduction_task(data, target)
 
 
 # 距离的度量
